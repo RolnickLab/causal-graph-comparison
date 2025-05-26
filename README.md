05af0fc33c72cf23a92c3968d9ca84d7ab7caf8c
Christina Isaicu <cisaicu@gmail.com>
2025-05-26 13:35:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Causal graph comparison 
+
+## Installation
+
+```python
+pip install -e . # Install the package in editable mode
+```
+
+## Usage instructions
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='cgc',
+   version='1.0',
+   description='Causal graph comparison for climate models',
+   author='Christina Isaicu',
+   author_email='c.isaicu@gmail.com',
+   packages=['cgc'],  #same as name
+   install_requires=['numpy'], #external packages as dependencies
+)
